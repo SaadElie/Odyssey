cfc81fdbaf4e35ee240f6f9d13689a236db184b9
Laura Kurtzberg <laurak@Lauras-Mac-mini.local>
2024-04-01 14:44:17 -0400

    updates to styles, content, and config

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ var config = {
             id: 'Introduction',
             alignment: 'right',
             title: 'Welcome to the Odyssey 4.0',
-            description: 'The gods are outraged, locked in conflict amongst themselves, and their celestial battles spill onto the Earth. The Trojan War not only cleaved mortals apart but also fractured the unity of the divine pantheon. Wielding their mighty powers, they reshape the very fabric of the world we know. Poseidon, with aspirations of extending his dominion, commands the waters to rise, asserting his influence over the seas. Zeus, in response, conjures more potent storms, countering the efforts of Demeter, who nurtures the land. Hera, too, employs her divine authority to disrupt birthrates and marriages among the supporters of rival deities. Athena seizes upon the chaos to foment conflict and spread the flames of war. Meanwhile, Hades sits upon his infernal throne, the gates of the underworld yawning wide to receive the flood of refugees fleeing the turmoil of the surface world. In this unprecedented exodus, the lower realms offer an escape, a refuge from the trials of the upper world. The balance of power has shifted, and the realm below emerges as a sanctuary, casting its somber allure over those seeking solace.',
+            description: 'The gods, outraged and locked in conflict amongst themselves, spill their celestial battles onto Earth. The Trojan War, a devastating event for mortals, also fractures the unity of the divine pantheon. With their mighty powers, they reshape the world we know. Poseidon, aspiring to extend his dominion, commands the waters to rise, asserting influence over the seas. Zeus, in response, conjures more potent storms, countering the efforts of Demeter, who nurtures the land. Hera, too, employs her divine authority to disrupt birthrates and marriages amongst the supporters of rival deities. Athena seizes upon the chaos to foment conflict and spread the flames of war. Meanwhile, Hades sits upon his infernal throne, the gates of the underworld yawning wide to receive the flood of refugees fleeing the turmoil of the surface world. In this unprecedented exodus, the lower realms offer escape and refuge from the trials of the upper world. The balance of power shifts, and the realm below emerges as a sanctuary, casting its somber allure over those seeking solace.',
             location: {
-                center: [15.898, 36.647],
+                center: [39.8, 22.2],
                 zoom: 4.5,
                 pitch: 0.00,
                 bearing: 0.00
@@ -56,11 +56,38 @@ var config = {
 
         },
         {
+            id: 'Introduction A',
+            alignment: 'left',
+            title: 'Everything to get home',
+            image: '',
+            description: "Meanwhile, on the mortal lands, one of the longest and deadliest wars comes to an end. After ten years, the Trojan War reaches its conclusion, and Odysseus's intricate plan proves successful. Cunningly constructing the deceptive horse while sacrificing a portion of the Greek fleet, they achieve victory. However, a decade is a period profound enough to transform the world. The lush forest canopy that once adorned Greece has dwindled, and the resources needed to <a href='https://joint-research-centre.ec.europa.eu/jrc-news-and-updates/scientists-estimate-2095-forest-services-will-decrease-average-52-europes-mediterranean-area-2023-04-20_en'>rebuild the fleet and embark on the journey home have become scarce</a>. Former allies now stand as adversaries as Greek commanders clash, staining the sun-kissed shores of Troy with fresh blood. Everyone seeks new resources to build a fleet for the journey home.",
+            location: {
+                center: [26.293, 39.963],
+                zoom: 6.62,
+                pitch: 55.50,
+                bearing: -7.20
+            },
+        },
+        {
             id: 'Chapter IA',
             alignment: 'left',
-            title: 'HOME',
+            title: 'Thrace (Land of the Cicones)',
             image: '',
-            description: 'The Trojan War reached its conclusion, and Odysseus\'s\ intricate plan proved successful. They cunningly constructed the deceptive horse even as they sacrificed a portion of the Greek fleet. However, a decade is a span of time profound enough to transform the very world. The lush forest canopy that once adorned Greece has dwindled by XX%. The resources needed for rebuilding the fleet and embarking on the journey home have become scarce. Those who were allies just yesterday now stand as adversaries, as Greek commanders clash, staining the sun-kissed shores of Troy with fresh blood. Employing yet another artful stratagem, Odysseus orchestrates the dissolution of the Greek right wing.',
+            description: "<p>So they set sail, departing for Ithaca, their hometown, a marvelous and prosperous city embraced by lavish greenery. Surprisingly, a storm strikes them, diverting their course and taking them to Thrace, the Land of the Cicones.</p> <em>When I had set sail thence the wind took me first to Ismarus, which is the city of the Cicons. There I sacked the town and put the people to the sword. We took their wives and also much booty, which we divided equitably amongst us so that none might have reason to complain. I then said that we had better make off at once, but my men very foolishly would not obey me, so they stayed there <span class='previous'>drinking much wine</span><span class='newtext'> drinking wine, not knowing that it is much stronger with a more acidic taste</span> and killing great numbers of sheep and oxen on the seashore.</em>",
+            location: {
+                center: [26.293, 39.963],
+                zoom: 6.62,
+                pitch: 55.50,
+                bearing: -7.20
+            },
+
+        },
+        {
+            id: 'Chapter IAA',
+            alignment: 'left',
+            title: '',
+            image: '',
+            description: 'Battered yet determined, Odysseus rallied his men and set sail once more. Likely navigating south along the Thracian coast, they sought refuge in calmer waters to regroup. Trusting the tranquil Mediterranean climate, they surrendered to the guidance of the wind. However, the capricious breeze led them astray to the land of the lotus-eaters.',
             location: {
                 center: [26.293, 39.963],
                 zoom: 6.62,
@@ -87,9 +114,9 @@ var config = {
         {
             id: 'Chapter IAB',
             alignment: 'left',
-            title: 'HOME',
+            title: 'Lotus Eaters',
             image: '',
-            description: 'He bides his time, waiting for the cloak of night to fall before dispatching his men to execute a stealthy maneuver: the substitution of olive oil with XXXX. The soldiers of the right wing, once nourished by the richness of olive oil, have long been deprived of its taste. This precious substance has become a rarity in the wake of the gods\'\ tumultuous conflict; its production plummeted, rendering it scarce. Having successfully purloined a ship, Odysseus and his steadfast crew set sail for Ithaca. Their journey unfolds aboard a vessel crafted from timber that never had the chance to reach its full glory, hewn prematurely from trees denied the opportunity to unveil their complete strength. ',
+            description: "<em><span class='previous'>I was driven thence by foul winds for a space of nine days upon the sea, but on the tenth day</span> we reached the land of the Lotus-eaters, who live on a food that comes from a kind of flower. <span class='previous'>Here we landed to take in fresh water</span>, and our crews got their mid-day meal on the shore near the ships. <span class='newtext'>Gone are the days when this journey would have taken a few days. Anemoi, the Greek god of wind, is exhausted by the wars of Olympus, and <a href='https://e360.yale.edu/features/global-stilling-is-climate-change-slowing-the-worlds-wind'>barely throws enough wind power</a> to move the ship. The crew is exhausted, and we even lost some men on this long trip. Here we landed, hoping to get some fresh water, however, <a href='https://www.unep.org/unepmap/resources/factsheets/climate-change'>only a few streams were left on the island</a>, heavily guarded. The men had to give up all their golden coins and some of their weapons to refill their water pouches, and had to taste the forbidden lotus.</span></em>",
             location: {
                 center: [24.8420, 38.8403],
                 zoom: 8.62,
@@ -248,7 +275,7 @@ var config = {
             id: 'Chapter VII',
             alignment: 'right',
             title: 'World 4.0',
-            description: 'What about you ? would you be able to survive the World 4.0 ? This word is not that much of a iagination as you think. The war between the gods is actually based on the changes that a +4 degrees of the surface of the Earth will bring upon our wrold.',
+            description: 'What about you ? would you be able to survive the World 4.0 ? This word is not that much of a imagination as you think. The war between the gods is actually based on the changes that a +4 degrees of the surface of the Earth will bring upon our wrold.',
             location: {
                 center: [15.8125, 35.5812],
                 zoom: 11,
